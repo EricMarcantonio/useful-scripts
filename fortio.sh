a0ee877b147351556335f3400a8317fd6776556a
Eric Marcantonio <eric11marcantonio@gmail.com>
2022-06-23 14:05:45 -0400

    Update fortio.sh

--- fortio.sh
+++ fortio.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -O https://dl.google.com/go/go1.18.3.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
-echo export PATH=$PATH:/usr/local/go/bin >> .bashrc
-echo export PATH=$PATH:/root/go/bin >> .bashrc
+echo "export PATH=$PATH:/usr/local/go/bin" >> .bashrc
+echo "export PATH=$PATH:/root/go/bin" >> .bashrc
 source .bashrc
 go install fortio.org/fortio@latest
