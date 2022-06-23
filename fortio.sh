a4919e36fb76567511f61f1939911051912d8f8e
Eric Marcantonio <eric11marcantonio@gmail.com>
2022-06-23 13:53:43 -0400

    Create fortio.sh

---
+++ fortio.sh
@@ -0,0 +1,4 @@
+curl -O https://dl.google.com/go/go1.18.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+echo export PATH=$PATH:/usr/local/go/bin >> .bashrc
+echo export PATH=$PATH:/root/go/bin >> .bashrc
